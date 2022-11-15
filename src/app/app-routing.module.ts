b62bd7a831ad85a6f10cd23db0410bc2cb9ae06f
alizain5663 <malikzain013@gmail.com>
2022-11-15 11:16:40 +0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'admin-module', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) }, { path: '', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, { path: 'management-module', loadChildren: () => import('./management-module/management-module.module').then(m => m.ManagementModuleModule) }];
+const routes: Routes = [
+  { path: 'admin-module', loadChildren: () => import('./admin-module/admin-module.module').then(m => m.AdminModuleModule) },
+   { path: '', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, 
+   { path: 'main-module', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) }, 
+   { path: 'management-module', loadChildren: () => import('./management-module/management-module.module').then(m => m.ManagementModuleModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
