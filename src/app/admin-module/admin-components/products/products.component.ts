1a62563bf3f309d84498849509df5258bc47efea
alizain5663 <malikzain013@gmail.com>
2022-11-18 17:23:36 +0500

    add

--- src/app/admin-module/admin-components/products/products.component.ts
+++ src/app/admin-module/admin-components/products/products.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/Shared/Services/product.service';
+
 
 @Component({
   selector: 'app-products',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+@ViewChild('FileSelect') FileSelect:ElementRef |any;
+color= ["Red" ,"Blue", "Black", "Pink"];
+selectSize=["S" ,"M",  "L", "X-L" , "XX-L"];
+ categories=["Cap", "Hoodies", "Watch", "Bags"];
+ newSizeArray:any=[];
+ imageArray: any = [];
+ disableButtonTrue: boolean = false;
+ myProductForm: FormGroup | any;
 
-  constructor() { }
+  constructor(
+    private ToastrService: ToastrService,
+    private formBUilder:FormBuilder,
+    private productService: ProductService
+  
+  ) {this.buidlForm() }
 
   ngOnInit(): void {
   }
+ buidlForm(){
+  this.myProductForm = this.formBUilder.group({
+    productName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
+    quantity: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]*$/)]),
+    price: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]*$/)]),
+    description: new FormControl('', Validators.required),
+    color: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
+    companyName: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
+    category: new FormControl('', Validators.required),
+    size: new FormArray([]),
+    productMaterial: new FormControl('', Validators.required)
+  })
+ }
 
 }
--- src/app/admin-module/admin-module.module.ts
+++ src/app/admin-module/admin-module.module.ts
@@ -6,6 +6,7 @@ import { AdminModuleComponent } from './admin-module.component';
 import { DashboardComponent } from './admin-components/dashboard/dashboard.component';
 import { ProductsComponent } from './admin-components/products/products.component';
 import { ProductsAnalyticsComponent } from './admin-components/products-analytics/products-analytics.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -17,7 +18,9 @@ import { ProductsAnalyticsComponent } from './admin-components/products-analytic
   ],
   imports: [
     CommonModule,
-    AdminModuleRoutingModule
+    AdminModuleRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class AdminModuleModule { }
